f96f95f0793ae24df0f808db380a8d7af5a18e1a
taho101 <tahirski@gmail.com>
2015-07-01 21:26:51 +0200

    Function is constructed

--- src/com/java/mappings/JavaScriptMapper.java
+++ src/com/java/mappings/JavaScriptMapper.java
@@ -2,7 +2,10 @@ package com.java.mappings;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 
 public class JavaScriptMapper extends Mapper {
 	
@@ -25,15 +28,22 @@ public class JavaScriptMapper extends Mapper {
 		
 		StringBuilder functionBuilder = new StringBuilder();
 		
-		functionBuilder.append(this.openFunction(info[0]));
+		functionBuilder.append(this.openFunction(info[0]) + this.newline);
+		
+		for(int i = 1; i < info.length; i++){
+			functionBuilder.append(this.applyMappings(info[i]) + this.newline);
+		}
 		
 		System.out.println(functionBuilder.toString());
+	}
+	
+	public void mapVariable(String var){
+		
 	}
 	
 	private String openFunction(String begin) {
 		String[] items = begin.split(" ");
 		String fName = ""; 
-		List<String> variables = new ArrayList<String>();
 		
 		if(items[0].equals("var") || items[0].equals("function")){
 			fName = (items[1].indexOf("(") > -1 ) ? items[1] : items[1].substring(0, items[1].indexOf("(") - 1);
@@ -41,14 +51,7 @@ public class JavaScriptMapper extends Mapper {
 			fName = items[0].substring(items[0].indexOf(".") + 1, items[0].length());
 		}
 		
-		//form the variables
-		if(items[0].equals("function")){
-			
-		}else{
-			for(int i = 1; i < items.length; i++){
-				
-			}
-		}
+		List<String> variables = this.addVariables(items);
 		
 		//combine variables
 		StringBuilder parameters = new StringBuilder();
@@ -59,11 +62,65 @@ public class JavaScriptMapper extends Mapper {
 		
 		return "public void "+ fName +"("+ parameters.toString() +"){ " + this.newline;
 	}
-
-	public void mapVariable(String var){
+	
+	//Extract variables from string array
+	private List<String> addVariables(String[] items){
+		List<String> variables = new ArrayList<String>();
 		
+		//form the variables
+		boolean varStart = false;
+		for(int i = 0; i < items.length; i++){
+			if(items[i].contains("(")){
+				varStart = true;
+				
+				String possibleVar = items[i].substring(items[i].indexOf("(") + 1, items[i].length()).replaceAll("/\\*.*\\*/", "");
+				if(possibleVar.length() > 0){
+					if(possibleVar.contains(",")){
+						String[] vars = possibleVar.split(",");
+						
+						for(int j = 0; j < vars.length; j++)
+							variables.add(vars[i]);
+							
+					}else{
+						variables.add(possibleVar);
+					}
+				}
+			}else if(varStart == true){
+				if(items[i].contains(")")){
+					if(items[i].indexOf(")") > 0){
+						String possibleVar = items[i].substring(0, items[i].indexOf(")")).replaceAll("/\\*.*\\*/", "");
+						
+						if(possibleVar.length() > 0){
+							if(possibleVar.contains(",")){
+								String[] vars = possibleVar.split(",");
+								
+								for(int j = 0; j < vars.length; j++)
+									variables.add(vars[i]);
+									
+							}else{
+								variables.add(possibleVar);
+							}
+						}
+					}
+					
+					varStart = false;
+				}
+			}
+		}
+		
+		return variables;
 	}
 	
+	private String applyMappings(String text){
+		Iterator<Entry<String, String>> it = this.mappings.entrySet().iterator();
+		
+		while (it.hasNext()) {
+	        Entry<String, String> pair = it.next();
+	        text = text.replace(pair.getKey(), pair.getValue());
+	    }
+		
+		return text;
+	}
 	
 	/*
 	 * Get functions for the completed mapped elements
